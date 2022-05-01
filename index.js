af5270ffb2e7c422de0b27a83004a98c0be870e1
Aletha Wheeless <augustkama98@gmail.com>
2022-04-30 14:17:50 -1000

    commit tree 3501080981fa153fe922e7f087b334e885c83612

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+const event = 'surprise'
+function writeCards(names, event) {
+    let messages = [];
+  for (let i = 0; i < names.length; i++) {
+   messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    console.log(messages);
+    return messages;
+}
+
+writeCards();
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+    return num;
+}
+countDown();
